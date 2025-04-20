01c4354bafddc21c3468304fd8d2e8f7c9d3f307
em-1001 <80628552+em-1001@users.noreply.github.com>
2025-04-20 17:49:46 +0900

    [Silver III] Title: N과 M (3), Time: 420 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include<cmath>
+
+using namespace std;
+
+vector<int> v;
+int n, m;
+
+bool promising(int i){
+    return 1;
+}
+
+void backtrack(int cdx){
+    if(cdx == m){
+        for(int i=0; i<v.size(); i++){
+            cout << v[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    
+    for(int i=1; i<=n; i++){
+        v.push_back(i);
+        if(promising(i)){
+            backtrack(cdx + 1);
+        }
+        v.pop_back();
+    }
+}
+
+int main()
+{
+    cin >> n >> m;
+    
+    backtrack(0);
+
+    return 0;
+}
