e6623cae7a10957584f313516ed54071e9a2ea19
em-1001 <80628552+em-1001@users.noreply.github.com>
2025-04-26 18:25:40 +0900

    [Silver II] Title: 미로 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1347. 미로 만들기/미로 만들기.cc
@@ -0,0 +1,77 @@
+#include <limits.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#define U 2
+#define D 0
+#define L 1
+#define R 3
+using namespace std;
+
+int dir = D;
+string map;
+
+int rx[51] = {0, };
+int ry[51] = {0, };
+int cx=0, cy=0;
+int F_cnt = 0;
+int max_x=0, min_x=0, max_y=0, min_y=0;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+    cin.tie(0);
+    int l;
+    cin >> l;
+    cin >> map;
+    rx[0] = 0;
+    ry[0] = 0;
+    for(int i=0; i<l; i++){
+        if(map[i] == 'L') dir = dir?(dir-1):3;
+        else if(map[i] == 'R') dir = (dir+1)%4;
+        else{
+            if(dir == U) cy -= 1;
+            else if(dir == D) cy += 1;
+            else if(dir == L) cx -= 1;
+            else cx += 1;
+            F_cnt++;
+            rx[F_cnt] = cx;
+            ry[F_cnt] = cy;
+            if(max_x < cx) max_x = cx;
+            if(min_x > cx) min_x = cx;
+            if(max_y < cy) max_y = cy;
+            if(min_y > cy) min_y = cy;
+        } 
+    }
+    if(min_x < 0){
+        for(int i=0; i<=F_cnt; i++){
+            rx[i] -= min_x;
+        }
+        max_x -= min_x;
+    }
+    if(min_y < 0){
+        for(int i=0; i<=F_cnt; i++){
+            ry[i] -= min_y;
+        }
+        max_y -= min_y;
+    }
+    
+    bool flag=1;
+    for(int i=0; i<=max_y; i++){
+        for(int j=0; j<=max_x; j++){
+            for(int k=0; k<=F_cnt; k++){
+                if(rx[k]==j && ry[k]==i){
+                    cout << '.';
+                    flag=0;
+                    break;
+                }
+            }
+            if(flag) cout << '#';
+            flag=1;
+        }
+        cout << '\n';
+    }
+    
+    return 0;
+}
