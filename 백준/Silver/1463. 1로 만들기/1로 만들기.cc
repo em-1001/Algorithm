d0d63a1f32a1d5b8c079ff096708a5639e8be5f3
em-1001 <80628552+em-1001@users.noreply.github.com>
2025-05-06 18:54:46 +0900

    [Silver III] Title: 1로 만들기, Time: 0 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -10,7 +10,7 @@ int dp[1000001];
 int MakeOne(int i){
     if(i==1) return 0; 
     if(dp[i]>0) return dp[i];
-    if(i%3==0 && i%2==0) return dp[i] = min({1+MakeOne(i/3), 1+MakeOne(i/2), 1+MakeOne(i-1)});
+    if(i%3==0 && i%2==0) return dp[i] = min(1+MakeOne(i/3), 1+MakeOne(i/2));
     else if(i%3==0) return dp[i] = min(1+MakeOne(i/3), 1+MakeOne(i-1));
     else if(i%2==0) return dp[i] = min(1+MakeOne(i/2), 1+MakeOne(i-1));
     else return dp[i] = 1+MakeOne(i-1);
