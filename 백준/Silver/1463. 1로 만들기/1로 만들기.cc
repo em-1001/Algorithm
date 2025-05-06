ebe43e25838c503e19dd7b2c85161614bf7f87ab
em-1001 <80628552+em-1001@users.noreply.github.com>
2025-05-06 18:49:34 +0900

    [Silver III] Title: 1로 만들기, Time: 28 ms, Memory: 29244 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <string.h>
+#include <algorithm>
+
+using namespace std; 
+int n; 
+int dp[1000001];
+
+int MakeOne(int i){
+    if(i==1) return 0; 
+    if(dp[i]>0) return dp[i];
+    if(i%3==0 && i%2==0) return dp[i] = min({1+MakeOne(i/3), 1+MakeOne(i/2), 1+MakeOne(i-1)});
+    else if(i%3==0) return dp[i] = min(1+MakeOne(i/3), 1+MakeOne(i-1));
+    else if(i%2==0) return dp[i] = min(1+MakeOne(i/2), 1+MakeOne(i-1));
+    else return dp[i] = 1+MakeOne(i-1);
+}
+
+int main()
+{
+    cin >> n; 
+    memset(dp, 0, sizeof(dp));
+    dp[1] = 0; 
+    dp[2] = 1; 
+    dp[3] = 1; 
+    dp[4] = 2; 
+    
+    int ans = MakeOne(n);
+    
+    /*
+    for(int i=1; i<=n; i++){
+        cout << dp[i] << " "; 
+    }*/
+    
+    cout << ans; 
+
+    return 0;
+}
