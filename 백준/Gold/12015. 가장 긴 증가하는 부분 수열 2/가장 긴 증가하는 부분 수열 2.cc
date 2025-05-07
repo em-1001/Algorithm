fa66b5c8b73cfe6221a80dc9f238321222c06355
em-1001 <80628552+em-1001@users.noreply.github.com>
2025-05-07 22:13:40 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 452 ms, Memory: 13740 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int in[1000001];
+int dp[1000001];
+int bs[1000001];
+
+int BinarySearch(int l, int r, int t){
+    int m;
+    while(l<r){
+        m = l+r>>1;
+        if(t==bs[m]) return m;
+        else if(t>bs[m-1] && t<bs[m]) return m;
+        else if(t>bs[m]) l=m+1;
+        else r=m-1;
+    }
+    return l;
+}
+
+int main()
+{
+    int n; 
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> in[i];
+        dp[i]=1;
+    }
+    
+    bs[0]=in[0];
+    int i=1, j=0;
+    while(i<n){
+        if(in[i]>bs[j]){
+            bs[j+1]=in[i];
+            dp[j+1]=dp[j]+1;
+            j++;
+        }
+        else{
+            int idx=BinarySearch(0, j, in[i]);
+            bs[idx]=in[i]; 
+        }
+        i++;
+    }
+    
+    cout << dp[j];
+
+    return 0;
+}
