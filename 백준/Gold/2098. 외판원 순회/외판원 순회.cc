cc8857a3e6f4210541bed7155d4d0c14df460098
em-1001 <80628552+em-1001@users.noreply.github.com>
2025-05-20 21:56:17 +0900

    [Gold I] Title: 외판원 순회, Time: 20 ms, Memory: 6116 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.cc
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.cc
@@ -13,13 +13,12 @@ int n, inf=pow(10,9);
 int TSP(int here, int visited){
     if(visited==(1<<n)-1) return (map[here][0])?map[here][0]:inf;
     if(dp[here][visited]!=-1) return dp[here][visited];
+    dp[here][visited]=inf;
     for(int i=0; i<n; i++){
         if(!(visited&(1<<i)) && map[here][i]!=0){
-            if(dp[here][visited]==-1) dp[here][visited]=map[here][i]+TSP(i, visited|(1<<i));
-            else dp[here][visited]=min(map[here][i]+TSP(i, visited|(1<<i)), dp[here][visited]);
+            dp[here][visited]=min(map[here][i]+TSP(i, visited|(1<<i)), dp[here][visited]);
         }
     }
-    if(dp[here][visited]==-1) dp[here][visited]=inf;
     return dp[here][visited];
 }
 
